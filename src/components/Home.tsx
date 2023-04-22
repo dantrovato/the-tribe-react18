95c78672b5399a191beaf1ad1fc93122989f425f
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-22 23:17:37 +0100

    Add padding to the p in home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <p className="col-lg-8 mx-auto fs-5 text-muted m-5">
+    <p className="col-lg-8 mx-auto fs-5 text-muted p-5">
       A social experiment in redifining, sex, friendships, relationships and
       love
     </p>
