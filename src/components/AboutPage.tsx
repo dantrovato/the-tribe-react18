c081865a558374dc0c5791ee4e7bc6d53d0dd739
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-23 13:55:50 +0100

    Correct typo in about

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
         Welcome to the Tribe, a community of hot kinksters in their 20's 30's.
         We host invites only parties of 100 or so guests every three months but
         we do smaller ones which focus on introducing newbies to the group. This
-        is s great space to meet like minded individuals who take good care of
+        is a great space to meet like minded individuals who take good care of
         their body and really spark joy. The community is precious to us so
         guests are carefully selected. It's not an easy place to be invited to
         but once you're in you're not gonna want to leave. So get in touch if
