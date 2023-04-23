063c9d6a2e3b21a650294e8dcab8ab4ec18870fd
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-23 03:15:05 +0100

    Add matilda and dan pic

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,24 @@
 const Home = () => {
+  const backgroundStyle = {
+    backgroundImage: "url(./matilda_dan.jpeg)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "46.5vh",
+    width: "100%",
+  };
   return (
-    <p className="col-lg-8 mx-auto fs-5 text-muted p-5">
-      A social experiment in redifining, sex, friendships, relationships and
-      love
-    </p>
+    <>
+      <p className="col-lg-8 mx-auto fs-5 text-muted p-5">
+        A social experiment in redifining, sex, friendships, relationships and
+        love
+      </p>
+      {/* <img
+        src="./matilda_dan.JPG"
+        alt=""
+        style={{ width: "100%", maxHeight: "40%" }}
+      /> */}
+      <div style={backgroundStyle}>bananarama</div>
+    </>
   );
 };
 
