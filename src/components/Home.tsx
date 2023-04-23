ba82f6864f4fa8fb9b0a7eedf411976dfa0e6808
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-23 03:29:47 +0100

    Swap background image on home component for img so that the size is responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,23 @@
 const Home = () => {
-  const backgroundStyle = {
-    backgroundImage: "url(./matilda_dan.jpeg)",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    height: "46.5vh",
-    width: "100%",
-  };
+  // const backgroundStyle = {
+  //   backgroundImage: "url(./matilda_dan.jpeg)",
+  //   backgroundSize: "cover",
+  //   backgroundRepeat: "no-repeat",
+  //   height: "46.5vh",
+  //   width: "100%",
+  // };
   return (
     <>
       <p className="col-lg-8 mx-auto fs-5 text-muted p-5">
         A social experiment in redifining, sex, friendships, relationships and
         love
       </p>
-      {/* <img
-        src="./matilda_dan.JPG"
+      <img
+        src="./matilda_dan.jpeg"
         alt=""
         style={{ width: "100%", maxHeight: "40%" }}
-      /> */}
-      <div style={backgroundStyle}>bananarama</div>
+      />
+      {/* <div style={backgroundStyle}>bananarama</div> */}
     </>
   );
 };
