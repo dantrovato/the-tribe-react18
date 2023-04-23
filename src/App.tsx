d49ba7df6309bad6c6bb51ba5348231b43b3e637
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-23 13:38:56 +0100

    Add zod validation to membership. It still doesn't show the error messages though

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Hero from "./components/Hero";
 import Home from "./components/Home";
 import { Footer } from "./components/Footer";
+import { FieldValues } from "react-hook-form";
 
 function App() {
+  const handleSubmit = (data: FieldValues) => {
+    const { firstName, lastName, email, socialMedia, phoneNumber } = data;
+    console.log(data);
+  };
   return (
     <BrowserRouter>
       <Navbar />
@@ -17,7 +22,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/membership" element={<Membership />} />
+        <Route
+          path="/membership"
+          element={<Membership onSubmit={handleSubmit} />}
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Membership.tsx
+++ src/components/Membership.tsx
@@ -1,75 +1,108 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface Props {
+  onSubmit: (data: FieldValues) => void;
+}
 
 const schema = z.object({
   firstName: z.string().min(2, { message: "Oi oi. 2 letters min" }),
   lastName: z.string().min(2, { message: "Oi oi. 3 letters min" }),
+  socialMedia: z.string().min(5, { message: "Oi oi. Is this valid??" }),
   email: z.string().email({ message: "Oi oi. Is that a valid email??" }),
   phoneNumber: z.string().regex(/^\+44\s?\d{10}$/),
 });
 
 type FormData = z.infer<typeof schema>;
 
-const Membership = () => {
+const Membership = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
+  const tempHandleSubmit = (data: FieldValues) => {
+    onSubmit(data);
+    reset();
+    alert(
+      "Site under construction. Details will not yet be added. Try again in a few days"
+    );
+  };
+
   return (
-    <form>
-      <div className="m-5">
-        <label htmlFor="firstName" className="form-label">
-          First Name
-        </label>
-        <input
-          {...register("firstName")}
-          id="firstName"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="m-5">
-        <label htmlFor="lastName" className="form-label">
-          Last Name
-        </label>
-        <input
-          {...register("lastName")}
-          id="lastName"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="m-5">
-        <label htmlFor="email" className="form-label">
-          Email
-        </label>
-        <input
-          {...register("email")}
-          id="email"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="m-5">
-        <label htmlFor="phoneNumber" className="form-label">
-          Phone Number
-        </label>
-        <input
-          {...register("phoneNumber")}
-          id="phoneNumber"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="m-5">
-        <button disabled={!isValid} className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-    </form>
+    <>
+      <h1 className="m-5 text-muted text-center">
+        Submit your details for membership
+      </h1>
+      <form onSubmit={(event) => tempHandleSubmit(event)}>
+        <div className="m-5">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+          </label>
+          <input
+            {...register("firstName")}
+            id="firstName"
+            type="text"
+            className="form-control"
+          />
+          {errors.firstName && (
+            <p className="text-danger">{errors.firstName.message}</p>
+          )}
+        </div>
+        <div className="m-5">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            {...register("lastName")}
+            id="lastName"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="socialMedia" className="form-label">
+            Social Media
+          </label>
+          <input
+            {...register("socialMedia")}
+            id="socialMedia"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            {...register("email")}
+            id="email"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="m-5">
+          <label htmlFor="phoneNumber" className="form-label">
+            Phone Number
+          </label>
+          <input
+            {...register("phoneNumber")}
+            id="phoneNumber"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="m-5">
+          <button disabled={!isValid} className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
