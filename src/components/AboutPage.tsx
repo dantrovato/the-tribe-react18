b0aa1251e3ef2231d8ce342a500187d634b042af
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-23 02:39:27 +0100

    Edit the about text

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -2,15 +2,14 @@ const AboutPage = () => {
   return (
     <div className="m-5 ">
       <p>
-        Welcome to the Tribe. We are a community of well over 100 kinksters,
-        mostly in their 20's 30's, who meet regularly for fun. The big parties
-        happen every three or four months but we have smaller ones which focus
-        on introducing newbies to the group. This is s great space to meet like
-        minded individuals who take good care of their body and really spark
-        joy. The community is precious to us so parties are mostly on an invite
-        only or recommendation basis, but once you're in you're not gonna want
-        to leave. So get in touch if you think you'll fit in and we look forward
-        to meet you :)
+        Welcome to the Tribe, a community of hot kinksters in their 20's 30's.
+        We host invites only parties of 100 or so guests every three months but
+        we do smaller ones which focus on introducing newbies to the group. This
+        is s great space to meet like minded individuals who take good care of
+        their body and really spark joy. The community is precious to us so
+        guests are carefully selected. It's not an easy place to be invited to
+        but once you're in you're not gonna want to leave. So get in touch if
+        you think you'll fit in and we look forward to getting to know you. x
       </p>
     </div>
   );
