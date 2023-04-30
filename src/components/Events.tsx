510309a79804937c4f8ca66d57a68134881fe62f
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-30 01:58:14 +0100

    Add link to rules in Events component and style text

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 const Events = () => {
   return (
     <div className="m-5">
       <h1 className="text-center text-muted">Events</h1>
       <p className="m-5 text-muted">
-        Next Event May 20th 2023 is a newbies. Register your interest in the
-        Join section.
+        Next Event, on May 20th 2023, is a smaller one and it's an intro for the
+        newbies. It will have roughly around 30 to 40 people.
+      </p>
+      <p className="m-5 text-muted">
+        Register your interest in the{" "}
+        <Link className="navbar-brand text-danger" to="/rules">
+          Rules
+        </Link>{" "}
+        section.
       </p>
       <img
         src="./girl_pink.jpg"
