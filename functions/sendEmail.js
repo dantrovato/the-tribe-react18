85b8c24fa9b7279ccf97f92e03c4036b54fac4c8
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-01 21:48:47 +0100

    Troubleshooting emails

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,27 +1,28 @@
 const sgMail = require("@sendgrid/mail");
 
 exports.handler = async (event, context) => {
-  const { firstName, lastName, email, socialMedia, phoneNumber } = JSON.parse(
-    event.body
-  );
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  const msg = {
-    to: "plasticsoup@yahoo.co.uk",
-    from: "dantrovato@gmail.com",
-    subject: "New form submission",
-    html: `
-      <p>First name: \${firstName}</p>
-      <p>Last name: \${lastName}</p>
-      <p>Email: \${email}</p>
-      <p>Social media: \${socialMedia}</p>
-      <p>Phone number: \${phoneNumber}</p>
-    `,
-  };
-
   try {
+    const { firstName, lastName, email, socialMedia, phoneNumber } = JSON.parse(
+      event.body
+    );
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: "plasticsoup@yahoo.co.uk",
+      from: "dantrovato@gmail.com",
+      subject: "New form submission",
+      html: `
+        <p>First name: ${firstName}</p>
+        <p>Last name: ${lastName}</p>
+        <p>Email: ${email}</p>
+        <p>Social media: ${socialMedia}</p>
+        <p>Phone number: ${phoneNumber}</p>
+      `,
+    };
+
     await sgMail.send(msg);
+
     return {
       statusCode: 200,
       body: "Email sent successfully",
