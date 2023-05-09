3a84a6fc56ca0d2b56e5a26fd7d822359f476db4
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-09 22:01:40 +0100

    Add background image to the google form

--- src/components/GoogleForm.tsx
+++ src/components/GoogleForm.tsx
@@ -2,19 +2,25 @@ import React from "react";
 
 class GoogleForm extends React.Component {
   render() {
+    const backgroundImage = {
+      backgroundImage: 'url("/two_girls_one_guy.jpeg")',
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "100vh",
+    };
+
     return (
-      <div className="d-flex justify-content-center">
-        <iframe
-          className="text-center"
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdO6YkMwKnIFKdD-KBIL80NGwRfVyO5ybjrlmSTZFGp8U3EnQ/viewform?embedded=true"
-          width="640"
-          height="1082"
-          // frameBorder="0"
-          // marginHeight="0"
-          // marginwidth="0"
-        >
-          Loading…
-        </iframe>
+      <div style={backgroundImage}>
+        <div className="d-flex justify-content-center">
+          <iframe
+            className="text-center"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdO6YkMwKnIFKdD-KBIL80NGwRfVyO5ybjrlmSTZFGp8U3EnQ/viewform?embedded=true"
+            width="640"
+            height="1082"
+          >
+            Loading…
+          </iframe>
+        </div>
       </div>
     );
   }
