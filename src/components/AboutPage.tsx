587f6e04f2aa222b7cac7025454b95c972b4a0bb
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-11 22:29:04 +0100

    Add members area, a page for individual events with a password required and add a max width to the div#root to 1000px

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -10,12 +10,13 @@ const AboutPage = () => {
       <p className="text-muted" style={{ maxWidth: "600px", margin: "auto" }}>
         Welcome to the Tribe, a community of hot and fun kinksters. We host
         invite-only parties of 100 or so guests every three months but we do
-        smaller ones which focus on introducing newbies to the group. This is a
-        great space to meet like minded individuals who take good care of their
-        body and really spark joy. This growing community is precious to us so
-        guests are carefully selected but once you're in you're not gonna want
-        to leave. So get in touch if you think you'll fit in and we look forward
-        to getting to know you
+        smaller ones in between which focus on introducing newbies to the group
+        as well as monthly socials. This is a great space to meet like-minded
+        individuals who take good care of their bodies and really spark joy.
+        This growing community is precious to us so guests are carefully
+        selected but once you're in you're not gonna want to leave. So get in
+        touch if you think you'll fit in and we look forward to getting to know
+        you
       </p>
     </div>
   );
