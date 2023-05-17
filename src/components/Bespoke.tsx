d10685afae81a1bb33117fa2a50a7e30d4e71093
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-17 21:27:43 +0100

    Redact bespoke fantasies text

--- src/components/Bespoke.tsx
+++ src/components/Bespoke.tsx
@@ -4,13 +4,13 @@ const Bespoke = () => {
   return (
     <div className="m-5">
       <h1 className="text-center text-muted mb-5">
-        Let us help fulfill your fantasy
+        Let us help fulfil your fantasy
       </h1>
       <p className="text-center text-muted mb-5">
         Whatever your kink, there's a decent chance somebody in the group will
-        be happy to realize this for you. If you don't ask you don't get. We
-        will never kink shame, unless your kink involves regetton. Then you will
-        be shamed severely. Tut tut. Imagine the shame of liking regetton!
+        be happy to indulge it for you. If you don't ask, you don't get. We will
+        never kink shame, unless your kink involves reggaeton. Then you will be
+        severely shamed. Tut tut. Imagine the shame of liking reggaeton!
       </p>
       <Footer />
     </div>
