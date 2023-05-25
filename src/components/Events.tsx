b2e378e561675e506a89a0fc5b398fafe8ce09da
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-25 11:59:44 +0100

    Remove the newbies from events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -5,46 +5,30 @@ const Events = () => {
   return (
     <div className="m-5">
       <h1 className="text-center text-muted mb-5">Next events</h1>
-      <h3 className="text-center text-muted mb-4">Newbies - May 20th 2023 </h3>
-      <p
-        className=" text-muted mb-3"
-        style={{ maxWidth: "600px", margin: "auto" }}
-      >
-        This is one of the smaller parties in Hornsey, N80JU, at the tribe
-        house, with hot tub and fire pit in the garden and it's a great intro
-        for the newbies. It will have roughly around 30 to 40 very attractive
-        and super friendly people. Remember there is never any pressure to get
-        involved so come and say hi :)
-      </p>
-      <p
-        className="text-muted mb-3"
-        style={{ maxWidth: "600px", margin: "auto" }}
-      >
-        Register your interest in the{" "}
-        <Link className="navbar-brand text-danger" to="/join">
-          Join
-        </Link>{" "}
-        section.
-      </p>
-      {/* <p className="text-muted">
-        View{" "}
-        <Link className="navbar-brand text-danger" to="/may_20_2023">
-          Event
-        </Link>{" "}
-      </p> */}
       <h3 className="text-center text-muted mb-4">Social - June 1st 2023 </h3>
       <p
         className=" text-muted mb-3"
         style={{ maxWidth: "600px", margin: "auto" }}
       >
-        This is a social at a bar in Camden. Location to be given later. Come
-        and meet some of our regular play mates and a lot of the new faces too.{" "}
+        This is a social at a bar in Camden. Location to be given to the guests
+        in the what's app group. Come and meet some of our regular play mates
+        and a lot of the new faces too.{" "}
         <a
           style={{ textDecoration: "none", color: `${danger}` }}
           href="https://www.meetup.com/meetup-group-hcdvbmyb/events/293473417/?utm_medium=referral&utm_campaign=share-btn_savedevents_share_modal&utm_source=link"
         >
           Join the Meet Up group
-        </a>
+        </a>{" "}
+        or register your interest in the{" "}
+        <p
+          className="text-muted mb-3"
+          style={{ maxWidth: "600px", margin: "auto" }}
+        >
+          <Link className="navbar-brand text-danger" to="/join">
+            Join
+          </Link>{" "}
+          section.
+        </p>
       </p>
       <h3 className="text-center text-muted mb-4">
         Tribeval - August 18th 2023{" "}
