aaaa8b0ed968a783baddd95598f1536b8da569c0
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-07-14 19:59:21 +0100

    Swap original main pic of matilda and dan for that of matilda and tamas

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,7 +18,8 @@ const Home = () => {
       </div> */}
 
       <img
-        src="./matilda_dan.jpeg"
+        // src="./matilda_dan.jpeg"
+        src="./matilda_tamas_cropped2.png"
         alt="Man and woman kissing"
         style={{ width: "100%", maxHeight: "40%" }}
       />
