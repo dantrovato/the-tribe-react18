1a816bc4ce8dfe4908c27c284f20a6948eaf7a3e
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-07-16 17:54:39 +0100

    Add lorna's testimonial and remove all double quotes

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -17,9 +17,9 @@ const Testimonials = () => {
           />
           <Carousel.Caption>
             <p>
-              "I came by myself the other day and instantly felt right at home.
+              I came by myself the other day and instantly felt right at home.
               Everyone’s so welcoming and lovely (and sexy as fuck). Had some of
-              the best experiences, such a hot night omg"
+              the best experiences, such a hot night omg
             </p>
             <h4>Hanna Chickpea</h4>
             <p>New member</p>
@@ -35,9 +35,9 @@ const Testimonials = () => {
           />
           <Carousel.Caption>
             <p className="">
-              "Hiiiii everyone, I’m Liv/Olivia - it was my first tribe party
-              last Friday and I had a lottttt of fun, genuinely couldn’t believe
-              how friendly and hot everybody was"
+              Hiiiii everyone, I’m Liv/Olivia - it was my first tribe party last
+              Friday and I had a lottttt of fun, genuinely couldn’t believe how
+              friendly and hot everybody was
             </p>
             <h4>Liv</h4>
             <p>New Member</p>
@@ -53,8 +53,8 @@ const Testimonials = () => {
           />
           <Carousel.Caption>
             <p className="">
-              "I always felt so welcome and safe that it became more like a
-              family than just a party."
+              I always felt so welcome and safe that it became more like a
+              family than just a party.
             </p>
             <h4>Carlota</h4>
             <p>Member since March 2021</p>
@@ -70,10 +70,9 @@ const Testimonials = () => {
           />
           <Carousel.Caption>
             <p className="">
-              “I went to my first Tribe Party in October and haven’t looked back
+              I went to my first Tribe Party in October and haven’t looked back
               since. It’s an incredibly liberating and welcoming group of
-              hotties. Can’t wait for many more adventures with this crazy
-              bunch”
+              hotties. Can’t wait for many more adventures with this crazy bunch
             </p>
             <h4>Matilda</h4>
             <p>Member since October 2022</p>
@@ -89,13 +88,32 @@ const Testimonials = () => {
           />
           <Carousel.Caption>
             <p className="">
-              "We were lucky enough to meet this amazing group of people. We
-              have had some amazing memories, made amazing friends, and could
-              not recommend more. Always a safe positive space for you to truly
-              be yourself with others alike"
+              We were lucky enough to meet this amazing group of people. We have
+              had some amazing memories, made amazing friends, and could not
+              recommend more. Always a safe positive space for you to truly be
+              yourself with others alike
             </p>
             <h4>Kyle and Lyd</h4>
             <p>New Member</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto"
+            src="./lorna.jpeg"
+            alt="Testimonial 5"
+            style={{ height: "30rem" }}
+          />
+          <Carousel.Caption>
+            <p className="">
+              'It is absurd to divide people into good and bad. People are
+              either charming or tedious' Oscar Wilde. The tribe are by far, far
+              from being tedious, instead, delectably charming at a minimum.
+              ❤️😘
+            </p>
+            <h4>Lorna</h4>
+            <p>Member since October 2022</p>
           </Carousel.Caption>
         </Carousel.Item>
 
@@ -107,7 +125,7 @@ const Testimonials = () => {
             style={{ height: "30rem" }}
           />
           <Carousel.Caption>
-            <p className="">"Chilling by the fire"</p>
+            <p className="">Chilling by the fire</p>
             <h4>Look</h4>
             <p>Look at the state of them!!!</p>
           </Carousel.Caption>
@@ -135,7 +153,7 @@ const Testimonials = () => {
             style={{ height: "30rem" }}
           />
           <Carousel.Caption>
-            <p className="">"He Man (They Mxn as of 2019)"</p>
+            <p className="">He Man (They Mxn as of 2019)</p>
             <h4>I am a fictional character</h4>
             <p>
               And my penis is entirely hypothetical. But if me and my male
@@ -152,7 +170,7 @@ const Testimonials = () => {
             style={{ height: "30rem" }}
           />
           <Carousel.Caption>
-            <p className="">"The tent and hot tub"</p>
+            <p className="">The tent and hot tub</p>
             <h4>Many a naughty has happened here</h4>
             <p>And many more to happen still</p>
           </Carousel.Caption>
