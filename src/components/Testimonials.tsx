b80f2c3608d3c9c4cf16902c1a8dd3e87e0bc3bf
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-07-17 22:52:01 +0100

    Refactor entire testimonials page to print each carousel dynamically using the new testimonials array of objects and the map method

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,253 +1,141 @@
 import { Carousel } from "react-bootstrap";
 import { Footer } from "./Footer";
 
+const testimonials = [
+  {
+    src: "./hannah_chickpea2.png",
+    alt: "A girl at a festival, laughing",
+    quote:
+      "I came by myself the other day and instantly felt right at home. Everyone’s so welcoming and lovely (and sexy as fuck). Had some of the best experiences, such a hot night omg",
+    name: "Hannah Chickpea",
+    member: "Member since May 2023",
+  },
+  {
+    src: "./liv_body.png",
+    alt: "Sexy girl body in lingerie",
+    quote:
+      "It was my first tribe party last Friday and I had a lottttt of fun, genuinely couldn’t believe how friendly and hot everybody was",
+    name: "Liv",
+    member: "New Member",
+  },
+  {
+    src: "./ruben.jpeg",
+    alt: "Handsome guy",
+    quote:
+      "The Tribe is an amazing group of people who I have created beautiful friendships with and and where I found a sense of community",
+    name: "Ruben",
+    member: "Member since May 2023",
+  },
+  {
+    src: "./shelly.jpeg",
+    alt: "Sexy girl in stockings",
+    quote:
+      "The Tribe is more than just another party, it’s a likeminded community who helped me overcome the insecurities I had about my own sexuality and begin to accept myself for who I am without judgement",
+    name: "Shelly",
+    member: "Member since 2020",
+  },
+  {
+    src: "./carlota1.jpg",
+    alt: "Sexy girl with a small roll up cigarette",
+    quote:
+      "I always felt so welcome and safe that it became more like a family than just a party.",
+    name: "Carlota",
+    member: "Member since 2020",
+  },
+  {
+    src: "./marco.jpeg",
+    alt: "Sexy man standing up",
+    quote:
+      "Passionate and intimate experience. You can express your sensuality and discover the lust and kink you may have hidden inside you.",
+    name: "Marco",
+    member: "Member since May 2023",
+  },
+  {
+    src: "./matilda_mirror_cropped.jpg",
+    alt: "Sexy woman in the mirror",
+    quote:
+      "I went to my first Tribe Party in October and haven’t looked back since. It’s an incredibly liberating and welcoming group of hotties. Can’t wait for many more adventures with this crazy bunch",
+    name: "Matilda",
+    member: "Member since October 2022",
+  },
+  {
+    src: "./dave_wolves.png",
+    alt: "Man surrounded by wolves",
+    quote:
+      "The thing that stands out to me about the Tribe are the friendships forged, the support given and the genuine love that flows throughout the group, during and after the events",
+    name: "Davide",
+    member: "Member since 2020",
+  },
+  {
+    src: "./kyle_lyd.png",
+    alt: "Man and woman in kinky outfit in front of mirror",
+    quote:
+      "We were lucky enough to meet this amazing group of people. We have had some amazing memories, made amazing friends, and could not recommend more. Always a safe positive space for you to truly be yourself with others alike",
+    name: "Kyle and Selene",
+    member: "Member since October 2022",
+  },
+  {
+    src: "./lorna_cropped.png",
+    alt: "Man and woman in kinky outfit in front of mirror",
+    quote:
+      "'It is absurd to divide people into good and bad. People are either charming or tedious' Oscar Wilde. The tribe are by far, far from being tedious, instead, delectably charming at a minimum.",
+    name: "Lorna",
+    member: "Member since October 2022",
+  },
+  {
+    src: "./tribe1.jpg",
+    alt: "Party people sitting on a sofa",
+    quote: "Chilling by the fire",
+    name: "LOOK",
+    member: "Look at the state of them!",
+  },
+  {
+    src: "./tribe2.jpg",
+    alt: "Party people sitting on a sofa",
+    quote: "",
+    name: "",
+    member: "And them!!!",
+  },
+  {
+    src: "./he_man.WEBP",
+    alt: "Cartoon character 'He Man'",
+    quote: "He Man (They Mxn as of 2019)",
+    name: "I am a fictional character",
+    member:
+      "And my penis is entirely hypothetical. But if me and my male member existed we'd be sure to join the tribe",
+  },
+  {
+    src: "./tribe_garden1.JPG",
+    alt: "Hot tub and tent in the garden",
+    quote: "The tent and hot tub",
+    name: "Many a naughty has happened here",
+    member: "And many more to happen still",
+  },
+];
+
 const Testimonials = () => {
   return (
     <div className="container bg-dark">
       <h5 className="text-center text-white p-4">
         Pics never shared without consent
       </h5>
+
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./hannah_chickpea2.png"
-            alt="Testimonial 1"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p>
-              I came by myself the other day and instantly felt right at home.
-              Everyone’s so welcoming and lovely (and sexy as fuck). Had some of
-              the best experiences, such a hot night omg
-            </p>
-            <h4>Hanna Chickpea</h4>
-            <p>New member</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./liv_body.png"
-            alt="Testimonial 2"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              Hiiiii everyone, I’m Liv/Olivia - it was my first tribe party last
-              Friday and I had a lottttt of fun, genuinely couldn’t believe how
-              friendly and hot everybody was
-            </p>
-            <h4>Liv</h4>
-            <p>New Member</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./ruben.jpeg"
-            alt="Handsome guy"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              The Tribe is an amazing group of people who I have created
-              beautiful friendships with and and where I found a sense of
-              community
-            </p>
-            <h4>Ruben</h4>
-            <p>Member since May 2023</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./shelly.jpeg"
-            alt="Gorgeous girl in sexy underwear"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              The Tribe is more than just another party, it’s a likeminded
-              community who helped me overcome the insecurities I had about my
-              own sexuality and begin to accept myself for who I am without
-              judgement
-            </p>
-            <h4>Shelly</h4>
-            <p>Member since 2021</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./carlota1.jpg"
-            alt="Testimonial 3"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              I always felt so welcome and safe that it became more like a
-              family than just a party.
-            </p>
-            <h4>Carlota</h4>
-            <p>Member since March 2021</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./marco.jpeg"
-            alt="Testimonial 3"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              Passionate and intimate experience. You can express your
-              sensuality and discover the lust and kink you may have hidden
-              inside you.
-            </p>
-            <h4>Marco</h4>
-            <p>Member since May 2023</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./matilda_mirror_cropped.jpg"
-            alt="Testimonial 4"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              I went to my first Tribe Party in October and haven’t looked back
-              since. It’s an incredibly liberating and welcoming group of
-              hotties. Can’t wait for many more adventures with this crazy bunch
-            </p>
-            <h4>Matilda</h4>
-            <p>Member since October 2022</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./dave_wolves.png"
-            alt="Mirror selfie"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              The thing that stands out to me about the Tribe are the
-              friendships forged, the support given and the genuine love that
-              flows throughout the group, during and after the events
-            </p>
-            <h4>Davide</h4>
-            <p>Member since 2020</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./kyle_lyd.png"
-            alt="Testimonial 5"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              We were lucky enough to meet this amazing group of people. We have
-              had some amazing memories, made amazing friends, and could not
-              recommend more. Always a safe positive space for you to truly be
-              yourself with others alike
-            </p>
-            <h4>Kyle and Lyd</h4>
-            <p>New Members</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./lorna_cropped.png"
-            alt="Black girl on her front"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">
-              'It is absurd to divide people into good and bad. People are
-              either charming or tedious' Oscar Wilde. The tribe are by far, far
-              from being tedious, instead, delectably charming at a minimum.
-              ❤️😘
-            </p>
-            <h4>Lorna</h4>
-            <p>Member since October 2022</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./tribe1.jpg"
-            alt="Group pic"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">Chilling by the fire</p>
-            <h4>Look</h4>
-            <p>Look at the state of them!!!</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./tribe2.jpg"
-            alt="Group pic"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            {/* <p className="">"Still chilling by the fire"</p> */}
-            {/* <h4>Look</h4> */}
-            <p>AND THEM!!!</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./he_man.WEBP"
-            alt="Hot tub and tent in the garden"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">He Man (They Mxn as of 2019)</p>
-            <h4>I am a fictional character</h4>
-            <p>
-              And my penis is entirely hypothetical. But if me and my male
-              member existed we'd be sure to join the tribe
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <img
-            className="d-block mx-auto"
-            src="./tribe_garden1.JPG"
-            alt="Hot tub and tent in the garden"
-            style={{ height: "30rem" }}
-          />
-          <Carousel.Caption>
-            <p className="">The tent and hot tub</p>
-            <h4>Many a naughty has happened here</h4>
-            <p>And many more to happen still</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {testimonials.map((testimonial, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block mx-auto"
+              src={testimonial.src}
+              alt={testimonial.alt}
+              style={{ height: "30rem" }}
+            />
+            <Carousel.Caption>
+              <p>{testimonial.quote}</p>
+              <h4>{testimonial.name}</h4>
+              <p>{testimonial.member}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
 
       <Footer />
