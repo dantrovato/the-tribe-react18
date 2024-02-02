82e23576142bafa74063b973f0bf490aa6492c64
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-02-02 14:58:19 +0000

    Center a link in the articles

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Articles = () => {
   return (
     <div className="m-5">
-      <h1 className="text-center text-muted p-5">Click on links below</h1>
+      <h1 className=" text-center text-muted p-5">Click on links below</h1>
       <a
-        className="navbar-brand text-muted p-5"
+        className="navbar-brand text-muted  d-block text-center"
         href="https://medium.com/@dantrovato/choking-the-gooch-c4ae28227ac7"
         target="_blank"
         rel="noopener noreferrer"
