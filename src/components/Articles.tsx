abcc57aef57dcc1fa464c82582fdd9e0cb05c15f
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-02-02 14:53:17 +0000

    Add link to medium to the articles page

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,72 +1,20 @@
 import { Footer } from "./Footer";
+import { Link } from "react-router-dom";
 
 const Articles = () => {
   return (
     <div className="m-5">
-      <h1 className="text-center text-muted mb-5">Choking the Gooch</h1>
-      <h2 className="text-center text-muted mb-5">
-        A guide to male multiple orgasms
-      </h2>
-      <h4 className="text-center text-muted mb-5">By Danimal</h4>
-      <p className="text-muted mb-5">
-        A few years ago I heard about a sexual practice involving semen
-        retention which may or may not have its roots in the tantric tradition
-        (I should probably look it up but can’t be bothered). My understanding
-        was that while orgasm and ejaculation happen at the same time they are
-        separate events, and by squeezing the gooch (perineum) muscles at the
-        point of climax, one can achieve orgasm while preventing semen from
-        leaving your body. <br /> “Why on earth would you want to do that?” you
-        may ask. Well, one reason would be shorter refractory time; a few
-        minutes and you’re good to go again. It’s a bit like the orgasms you
-        achieved when you were pre-pubescent; when you used to wank to your
-        heart’s content and no mess was ever made. Remember when you used to set
-        the alarm for two hours before you had to get up for school and rip the
-        head off it as if it owed you money? You used to knock eight or nine out
-        before your parents came to “wake you up”. It may have just been me but
-        the point is that, as a grown up male, it’s ejaculation, and not orgasm,
-        that leaves us knackered. <br /> <br />A second reason is that there is
-        evidence that, whilst sex increases our testosterone levels, ejaculation
-        actually crashes it. One way to naturally increase testosterone levels,
-        according to Dr Andrew Huberman among others, is to have sex but not
-        finish. Essentially pulling out before orgasm. That goes against my
-        principles. Also, in the immortal words of Billy Connolly, “At the point
-        of ejaculation there is no herd of horses that could get my arse go in
-        the other direction”. <br /> The benefits of semen retention for the
-        purpose of testosterone increase seem to peak at about one week, after
-        which time they remain constant. So ejaculating just once a week would
-        seem optimal for your testosterone levels. Well that doesn’t sound fun.{" "}
-        <br /> <br />
-        Chances are you, like me, are a bit of a sex monkey, and have
-        “commitments” to fulfil. When I first experimented with the squeezing of
-        the gooch, I found that it ruined my orgasm whilst only slightly
-        reducing the amount of sperm coming out of me. I quickly abandoned the
-        esoteric technique and dismissed it as almost unachievable and
-        unrealistic, the sexual equivalent of “get rich by watching this video
-        on drop shipping”. <br /> <br />A couple of months prior to writing this
-        badly cobbled together stream of consciousness, I started thinking about
-        it again and found some videos that gave me hope. Particularly one that
-        suggested a hack. This technique involved using your hands to squeeze
-        the perineum, and seemed much simpler than the hands-free version. I
-        thought I’d give it another go. So over the next few weeks I
-        experimented with it, alone or in good company, and found exactly what
-        works for me. Initially I ruined a few orgasms by pressing on the wrong
-        bits. Other times I just wasn’t able to stop jizz coming out by other
-        not pressing hard enough or pressing on the wrong parts. <br /> One of
-        my first mistakes was placing my fingers too far away from my anus. What
-        I found works for me best is to put my middle finger just above my
-        arsehole, where my prostate is, and the index finger just above that.
-        Then at the point of orgasm I squeeze pretty hard on the prostate. That
-        actually, massively INCREASES the intensity of my orgasm whilst
-        completely preventing any semen from coming out. It’s a great dry
-        sneeze. You take a short break and you can do this over and over again.
-        It’s mind-boggling. <br /> <br />I have been doing this ever since and
-        now only ejaculate once a week. When that happens you may want to have a
-        towel handy. But it’s been fascinating to see how much more sex I have
-        been able to have. I highly recommend this to anyone looking to increase
-        their sexual power naturally. I’m still experimenting with this and will
-        probably tweak this blog as the weeks go on. Anyways, if you do decide
-        to give it a go lemme know how you get on. Ok, get out.
-      </p>
+      <h1 className="text-center text-muted p-5">Click on links below</h1>
+      <a
+        className="navbar-brand text-muted p-5"
+        href="https://medium.com/@dantrovato/choking-the-gooch-c4ae28227ac7"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Choking the Gooch - A guide to male multiple orgasms - Written by
+        Danimal
+      </a>{" "}
+      <p className="text-center text-muted m-5"></p>
       <Footer />
     </div>
   );
