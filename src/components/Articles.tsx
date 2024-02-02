a817042509a60017d97494b9572c467d6b1f8831
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-02-02 15:02:01 +0000

    Fine tune positioning on link on articles

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Articles = () => {
   return (
     <div className="m-5">
-      <h1 className=" text-center text-muted p-5">Click on links below</h1>
+      <h1 className="d-block text-center text-muted">Click on links below</h1>
       <a
-        className="navbar-brand text-muted  d-block text-center"
+        className="navbar-brand text-muted  d-block text-center m-5"
         href="https://medium.com/@dantrovato/choking-the-gooch-c4ae28227ac7"
         target="_blank"
         rel="noopener noreferrer"
@@ -14,7 +14,6 @@ const Articles = () => {
         Choking the Gooch - A guide to male multiple orgasms - Written by
         Danimal
       </a>{" "}
-      <p className="text-center text-muted m-5"></p>
       <Footer />
     </div>
   );
