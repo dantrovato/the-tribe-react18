75c7270121430edb82550cc79308d9bb5ef945e2
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-03-10 14:49:33 +0000

    Add Denise and black and white kitchen shot to testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -6,7 +6,7 @@ const testimonials = [
     src: "./hannah_chickpea2.png",
     alt: "A girl at a festival, laughing",
     quote:
-      "I came by myself the other day and instantly felt right at home. Everyone’s so welcoming and lovely (and sexy as fuck). Had some of the best experiences, such a hot night omg",
+      '"I came by myself the other day and instantly felt right at home. Everyone’s so welcoming and lovely (and sexy as fuck). Had some of the best experiences, such a hot night omg"',
     name: "Hannah Chickpea",
     member: "Member since May 2023",
   },
@@ -14,7 +14,7 @@ const testimonials = [
     src: "./liv_body.png",
     alt: "Sexy girl body in lingerie",
     quote:
-      "It was my first tribe party last Friday and I had a lottttt of fun, genuinely couldn’t believe how friendly and hot everybody was",
+      '"It was my first tribe party last Friday and I had a lottttt of fun, genuinely couldn’t believe how friendly and hot everybody was"',
     name: "Liv",
     member: "New Member",
   },
@@ -22,7 +22,7 @@ const testimonials = [
     src: "./ruben.jpeg",
     alt: "Handsome guy",
     quote:
-      "The Tribe is an amazing group of people who I have created beautiful friendships with and and where I found a sense of community",
+      '"The Tribe is an amazing group of people who I have created beautiful friendships with and and where I found a sense of community"',
     name: "Ruben",
     member: "Member since May 2023",
   },
@@ -30,7 +30,7 @@ const testimonials = [
     src: "./shelly.jpeg",
     alt: "Sexy girl in stockings",
     quote:
-      "The Tribe is more than just another party, it’s a likeminded community who helped me overcome the insecurities I had about my own sexuality and begin to accept myself for who I am without judgement",
+      '"The Tribe is more than just another party, it’s a likeminded community who helped me overcome the insecurities I had about my own sexuality and begin to accept myself for who I am without judgement"',
     name: "Shelly",
     member: "Member since 2020",
   },
@@ -38,7 +38,7 @@ const testimonials = [
     src: "./carlota1.jpg",
     alt: "Sexy girl with a small roll up cigarette",
     quote:
-      "I always felt so welcome and safe that it became more like a family than just a party.",
+      '"I always felt so welcome and safe that it became more like a family than just a party"',
     name: "Carlota",
     member: "Member since 2020",
   },
@@ -46,7 +46,7 @@ const testimonials = [
     src: "./marco.jpeg",
     alt: "Sexy man standing up",
     quote:
-      "Passionate and intimate experience. You can express your sensuality and discover the lust and kink you may have hidden inside you.",
+      '"Passionate and intimate experience. You can express your sensuality and discover the lust and kink you may have hidden inside you"',
     name: "Marco",
     member: "Member since May 2023",
   },
@@ -54,7 +54,7 @@ const testimonials = [
     src: "./dave_wolves.png",
     alt: "Man surrounded by wolves",
     quote:
-      "The thing that stands out to me about the Tribe are the friendships forged, the support given and the genuine love that flows throughout the group, during and after the events",
+      '"The thing that stands out to me about the Tribe are the friendships forged, the support given and the genuine love that flows throughout the group, during and after the events"',
     name: "Davide",
     member: "Member since 2020",
   },
@@ -62,24 +62,33 @@ const testimonials = [
     src: "./demi.jpeg",
     alt: "Man surrounded by wolves",
     quote:
-      "Can't beat the cuddle puddle at the end of the night.. such a great group of people, always an amazing vibe",
+      '"Can\'t beat the cuddle puddle at the end of the night.. such a great group of people, always an amazing vibe"',
     name: "Demi",
     member: "Member since 2022",
   },
+
   {
-    src: "./kyle_lyd.png",
-    alt: "Man and woman in kinky outfit in front of mirror",
+    src: "./denise.jpeg",
+    alt: "woman in kinky outfit in front of mirror",
     quote:
-      "We were lucky enough to meet this amazing group of people. We have had some amazing memories, made amazing friends, and could not recommend more. Always a safe positive space for you to truly be yourself with others alike",
-    name: "Kyle and Selene",
-    member: "Member since October 2022",
+      '"Just wanted to say a HUGE thank you for last night! It was seriously incredible, and you all made my first tribe party absolutely magical. ✨Can\'t wait for the next event to see all your beautiful faces and asses again! x"',
+    name: "Denise",
+    member: "Member since March 2024",
   },
   {
     src: "./lorna_cropped.png",
     alt: "Man and woman in kinky outfit in front of mirror",
     quote:
-      "'It is absurd to divide people into good and bad. People are either charming or tedious' Oscar Wilde. The tribe are by far, far from being tedious, instead, delectably charming at a minimum.",
+      '"It is absurd to divide people into good and bad. People are either charming or tedious\' Oscar Wilde. The tribe are by far, far from being tedious, instead, delectably charming at a minimum."',
     name: "Lorna",
+    member: "Member since October 2022",
+  },
+  {
+    src: "./kyle_lyd.png",
+    alt: "Man and woman in kinky outfit in front of mirror",
+    quote:
+      '"We were lucky enough to meet this amazing group of people. We have had some amazing memories, made amazing friends, and could not recommend more. Always a safe positive space for you to truly be yourself with others alike"',
+    name: "Kyle and Selene",
     member: "Member since October 2022",
   },
   // {
@@ -97,12 +106,19 @@ const testimonials = [
   //   member: "And them!!!",
   // },
   {
+    src: "./wetblackwhite.jpeg",
+    alt: "Party people posing",
+    quote: "There's a shark in the room!!",
+    name: "\"Scenes from the 'WET' party\"",
+    member: "Wet dreams did come true!",
+  },
+  {
     src: "./he_man.WEBP",
     alt: "Cartoon character 'He Man'",
     quote: "He Man (They Mxn as of 2019)",
-    name: "I am a fictional character",
+    name: '"I am a fictional character...',
     member:
-      "And my penis is entirely hypothetical. But if me and my male member existed we'd be sure to join the tribe",
+      "...and my penis is entirely hypothetical. But if and my male member and I existed we'd be sure to join the tribe\"",
   },
   {
     src: "./tribe_garden1.JPG",
