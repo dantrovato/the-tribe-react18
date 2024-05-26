d5a534a33b55edd4a8d3909e8fb8c297ce8b68b1
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-05-26 17:29:54 +0100

    Add Rockstars pics to the gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -3,6 +3,66 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./Gallery.module.css";
 import { Footer } from "../Footer";
 
+const rockhard = [
+  { src: "../Rockhard/anotherprick.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/abbygradyrox.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/abbyroxkiss.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/abicourtney.jpeg", alt: "gitl in ballpit in uv room" },
+  {
+    src: "../Rockhard/arihollyroxrtizt.jpeg",
+    alt: "gitl in ballpit in uv room",
+  },
+  {
+    src: "../Rockhard/danroxhard.jpeg",
+    alt: "gitl in ballpit in uv room",
+  },
+  {
+    src: "../Rockhard/dimitriholliehug.jpeg",
+    alt: "gitl in ballpit in uv room",
+  },
+  {
+    src: "../Rockhard/dimitrispanksrox.jpeg",
+    alt: "gitl in ballpit in uv room",
+  },
+  { src: "../Rockhard/donnakikorox.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/donnamac.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/donnamac2.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/emmablur.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/epicceiling.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/girltrain.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/gradykikorox.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/hollielaugh.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/hollieroxrtizt.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/hollietamas.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/jamesjamie.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/jamierori.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/kikodj.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/kikoguns.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/kitchenchill.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/kitchencuddle.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/lindsayholdem.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/kitchenchill.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/linprismag.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/linprismag2.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/makguns.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/michaellicking.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/perigirl.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/perigirl2.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/posters1.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/posters2.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/posters3.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/posters4.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/posters5.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/prisbum.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/prisjames.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/roxsophie.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/sofiesnick.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/tamasjanecute.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/tamasjanekiss.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/tamasuendel.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../Rockhard/thegrope.jpeg", alt: "gitl in ballpit in uv room" },
+];
+
 const wet = [
   { src: ".././wetblackwhite.jpeg", alt: "party people" },
   { src: ".././chrisrtizt.jpeg", alt: "two men in neptune outfits" },
@@ -39,24 +99,30 @@ const wet = [
   { src: "../jamesp.jpeg", alt: "party boy" },
 ];
 const jungle = [
-  { src: "../seanlucy.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../chriskiko.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../chrisdemi.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/seanlucy.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/chriskiko.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/chrisdemi.jpeg", alt: "people in sexy sea themed outfits" },
   {
-    src: "../merlincarlotanegin.jpeg",
+    src: "../Jungle/merlincarlotanegin.jpeg",
     alt: "people in sexy sea themed outfits",
   },
-  { src: "../stairs.jpeg", alt: "stairs decorated like a jungle" },
-  { src: "../carlotamerlin.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../leonnegin.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../danjungle.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../junglesetup.jpeg", alt: "Jungle decorations" },
+  { src: "../Jungle/stairs.jpeg", alt: "stairs decorated like a jungle" },
   {
-    src: "../yenemmaapple.jpeg",
+    src: "../Jungle/carlotamerlin.jpeg",
+    alt: "people in sexy sea themed outfits",
+  },
+  { src: "../Jungle/leonnegin.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/danjungle.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/junglesetup.jpeg", alt: "Jungle decorations" },
+  {
+    src: "../Jungle/yenemmaapple.jpeg",
     alt: "people in sexy sea themed outfits biting apple together",
   },
-  { src: "../louisiana.jpeg", alt: "people in sexy sea themed outfits" },
-  { src: "../carlotajungle.jpeg", alt: "people in sexy sea themed outfits" },
+  { src: "../Jungle/louisiana.jpeg", alt: "people in sexy sea themed outfits" },
+  {
+    src: "../Jungle/carlotajungle.jpeg",
+    alt: "people in sexy sea themed outfits",
+  },
 ];
 
 const random = [
@@ -83,81 +149,119 @@ const Gallery = () => {
       <h4 className="text-center text-white p-5">
         Pics never shared without consent
       </h4>
-      <img
-        src="wet.png"
-        alt="Sexy girl semi naked holding beer over her breast"
-        className="mx-auto d-block mb-4"
-        style={{ maxWidth: "100%" }}
-      />
-      <h4 className="text-center text-white p-4">Wet dreams did come true</h4>
+      <div>
+        <img
+          src="rockstars.png"
+          alt="Sexy girl sticking tongue out"
+          className="mx-auto d-block mb-4"
+          style={{ maxWidth: "80%" }}
+        />
+        <h4 className="text-center text-white p-4">
+          Music makes the people (something something) together
+        </h4>
 
-      <Row>
-        {wet.map((image, index) => (
-          <Col md={3} key={index} className="text-center">
-            <div className="gallery-item">
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="img-fluid gallery-image"
-                style={{
-                  height: "350px",
-                  width: "270px",
-                  objectFit: "cover",
-                  padding: "1rem",
-                }} // Apply inline styles here
-              />
-            </div>
-          </Col>
-        ))}
+        <Row>
+          {rockhard.map((image, index) => (
+            <Col md={3} key={index} className="text-center">
+              <div className="gallery-item">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="img-fluid gallery-image"
+                  style={{
+                    height: "350px",
+                    width: "270px",
+                    objectFit: "cover",
+                    padding: "1rem",
+                  }}
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div>
+        <img
+          src="wet.png"
+          alt="Sexy girl under water"
+          className="mx-auto d-block mb-4"
+          style={{ maxWidth: "100%" }}
+        />
+        <h4 className="text-center text-white p-4">Wet dreams did come true</h4>
+
+        <Row>
+          {wet.map((image, index) => (
+            <Col md={3} key={index} className="text-center">
+              <div className="gallery-item">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="img-fluid gallery-image"
+                  style={{
+                    height: "350px",
+                    width: "270px",
+                    objectFit: "cover",
+                    padding: "1rem",
+                  }}
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div>
         <img
           src="jungle.jpeg"
-          alt="Sexy girl semi naked holding beer over her breast"
+          alt="Two sexy girls in the jungle"
           className="mx-auto d-block mb-4"
           style={{ maxWidth: "52rem", paddingTop: "2rem" }}
         />
         <h4 className="text-center text-white p-4">
           Welcome to the jungle October 2023
         </h4>
-      </Row>
-      <Row>
-        {jungle.map((image, index) => (
-          <Col md={3} key={index} className="text-center">
-            <div className="gallery-item">
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="img-fluid gallery-image"
-                style={{
-                  height: "350px",
-                  width: "270px",
-                  objectFit: "cover",
-                  padding: "1rem",
-                }} // Apply inline styles here
-              />
-            </div>
-          </Col>
-        ))}
-      </Row>
-      <h4 className="text-center text-white p-4">Random snaps</h4>
-      <Row>
-        {random.map((image, index) => (
-          <Col md={3} key={index} className="text-center">
-            <div className="gallery-item">
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="img-fluid gallery-image"
-                style={{
-                  height: "350px",
-                  width: "270px",
-                  objectFit: "cover",
-                  padding: "1rem",
-                }} // Apply inline styles here
-              />
-            </div>
-          </Col>
-        ))}
-      </Row>
+        <Row>
+          {jungle.map((image, index) => (
+            <Col md={3} key={index} className="text-center">
+              <div className="gallery-item">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="img-fluid gallery-image"
+                  style={{
+                    height: "350px",
+                    width: "270px",
+                    objectFit: "cover",
+                    padding: "1rem",
+                  }} // Apply inline styles here
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div>
+        <h4 className="text-center text-white p-4">Random snaps</h4>
+        <Row>
+          {random.map((image, index) => (
+            <Col md={3} key={index} className="text-center">
+              <div className="gallery-item">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="img-fluid gallery-image"
+                  style={{
+                    height: "350px",
+                    width: "270px",
+                    objectFit: "cover",
+                    padding: "1rem",
+                  }} // Apply inline styles here
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+
       <Footer />
     </Container>
   );
