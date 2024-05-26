2fcd8283c7cd38502dda9c2a19cc70cbc1d9a067
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-05-26 13:27:21 +0100

    Add catfish 5 to 8

--- src/components/Catfish/Catfish.tsx
+++ src/components/Catfish/Catfish.tsx
@@ -4,17 +4,22 @@ import "./Catfish.module.css";
 import { Footer } from "../Footer";
 
 const catfish = [
-  { src: "../catfish1.jpeg", alt: "gitl in ballpit in uv room" },
-  { src: "../catfish2.jpeg", alt: "gitl in ballpit in uv room" },
-  { src: "../catfish3.jpeg", alt: "gitl in ballpit in uv room" },
-  { src: "../catfish4.jpeg", alt: "gitl in ballpit in uv room" },
+  { src: "../catfish1.jpeg", alt: "Instagram account page" },
+  { src: "../catfish2.jpeg", alt: "Instagram account page" },
+  { src: "../catfish3.jpeg", alt: "Instagram account page" },
+  { src: "../catfish4.jpeg", alt: "Instagram account page" },
+  { src: "../catfish5.jpeg", alt: "Instagram account page" },
+  { src: "../catfish6.jpeg", alt: "Instagram account page" },
+  { src: "../catfish7.jpeg", alt: "Instagram account page" },
+  { src: "../catfish8.jpeg", alt: "Instagram account page" },
 ];
 
 const Catfish = () => {
   return (
     <Container className="bg-dark">
       <h4 className="text-center text-white p-5">
-        Stinky catfishing profiles caught on insta
+        Below is an inventory of stinky catfish or profiles reported to us as
+        suspect
       </h4>
 
       <Row>
