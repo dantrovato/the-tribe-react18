58c819fb8253e9759387404af109fff04422e127
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-11-07 22:45:57 +0000

    Add new widget for elfsight so that each widget isn't overloaded with too many pics

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -15,6 +15,15 @@ const Gallery = () => {
         async
       ></script>
       <div
+        className="elfsight-app-d0806375-7ca1-4645-b7c6-1cbcc688461c"
+        data-elfsight-app-lazy
+      ></div>
+
+      <script
+        src="https://static.elfsight.com/platform/platform.js"
+        async
+      ></script>
+      <div
         className="elfsight-app-15829d16-0a2c-446f-977f-163b48d30be7"
         data-elfsight-app-lazy
       ></div>
