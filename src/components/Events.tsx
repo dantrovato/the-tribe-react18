8a28f14d40af1fbfcc01d92733e191cf2b36e186
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-12-17 10:30:26 +0000

    Remove catfish folder. Update events with NYE flyer

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -7,31 +7,12 @@ const Events = () => {
 
       <div className="mx-auto text-center" style={{ maxWidth: "80%" }}>
         <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">Play Social</h3>
-          <h4 className="text-center text-muted mb-4">November 9th 2024 </h4>
-          <img
-            src="/play_social_website.png"
-            alt="Attractive people in a bar"
-            className="mx-auto d-block mb-4"
-            style={{ maxWidth: "100%", marginTop: "2rem" }}
-          />
-          <p
-            className="text-muted mb-3"
-            style={{ maxWidth: "600px", margin: "auto" }}
-          >
-            This is a chance to meet the gorgeous children of the Tribe before
-            the party on December 7th. This is a new venue with limited
-            availability. Play allowed. Shhh ;)
-          </p>
-        </div>
-
-        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
           <h3 className="text-center text-muted mb-4">
-            The Naughtivity - Hoe Hoe Hoe
+            The Eroticles of Narnia - The loin, the switch in the wardrobe
           </h3>
-          <h4 className="text-center text-muted mb-4">December 7th 2024 </h4>
+          <h4 className="text-center text-muted mb-4">New Year's Eve</h4>
           <img
-            src="/naughtivity_website.png"
+            src="/narniawebsite4.png"
             alt="Attractive people in a bar"
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
