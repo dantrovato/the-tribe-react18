d9896eea7f7c6f0371697ee8bc74e65005c9552b
Daniele Trovato <plasticsoup@yahoo.co.uk>
2024-12-25 23:40:03 +0000

    Add play social flyer for jan 16th

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -26,6 +26,24 @@ const Events = () => {
           </p>
         </div>
 
+        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
+          <h3 className="text-center text-muted mb-4">Play social</h3>
+          <h4 className="text-center text-muted mb-4">Thursday 16th January</h4>
+          <img
+            src="/play_socialwebsite.png"
+            alt="Attractive people in a bar"
+            className="mx-auto d-block mb-4"
+            style={{ maxWidth: "100%", marginTop: "2rem" }}
+          />
+          <p
+            className="text-muted mb-3"
+            style={{ maxWidth: "600px", margin: "auto" }}
+          >
+            Come and meet the gorgeous children of the tribe at a bar in East
+            London for a midweek social and a little more if the mood strikes
+          </p>
+        </div>
+
         <p
           className="text-muted mb-3"
           style={{ maxWidth: "600px", margin: "auto" }}
