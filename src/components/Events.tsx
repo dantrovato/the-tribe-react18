a24b53334c89627438512c6f6a80612151208c14
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-01-01 18:45:05 +0000

    Remove narnia and cultural appropriation poster

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -7,26 +7,6 @@ const Events = () => {
 
       <div className="mx-auto text-center" style={{ maxWidth: "80%" }}>
         <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">
-            The Eroticles of Narnia - The loin, the switch in the wardrobe
-          </h3>
-          <h4 className="text-center text-muted mb-4">New Year's Eve</h4>
-          <img
-            src="/narniawebsite4.png"
-            alt="Attractive people in a bar"
-            className="mx-auto d-block mb-4"
-            style={{ maxWidth: "100%", marginTop: "2rem" }}
-          />
-          <p
-            className="text-muted mb-3"
-            style={{ maxWidth: "600px", margin: "auto" }}
-          >
-            A fun filled night of great deep house, a huge ball pit, a hot tub,
-            two monitored play rooms and lots and lots of gorgeous people
-          </p>
-        </div>
-
-        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
           <h3 className="text-center text-muted mb-4">Play social</h3>
           <h4 className="text-center text-muted mb-4">Thursday 16th January</h4>
           <img
@@ -41,28 +21,6 @@ const Events = () => {
           >
             Come and meet the gorgeous children of the tribe at a bar in East
             London for a midweek social and a little more if the mood strikes
-          </p>
-        </div>
-
-        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">
-            The Tribeversary - Cultural appropriation
-          </h3>
-          <h4 className="text-center text-muted mb-4">Saturday 25th January</h4>
-          <img
-            src="/culturalwebsite2.png"
-            alt="Attractive people in a bar"
-            className="mx-auto d-block mb-4"
-            style={{ maxWidth: "100%", marginTop: "2rem" }}
-          />
-          <p
-            className="text-muted mb-3"
-            style={{ maxWidth: "600px", margin: "auto" }}
-          >
-            The Tribe’s 5th anniversary. As a celebration of 5 years together we
-            wanted to put together a theme that celebrates all of us. Let our
-            differences bring us together, instead of dividing us. One tribe,
-            one love x
           </p>
         </div>
 
