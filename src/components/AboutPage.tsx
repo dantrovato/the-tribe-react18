852580cdb7fda02a9e352af40bb36951df03e291
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-01-08 22:04:45 +0000

    Add spaces to the about page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -9,26 +9,25 @@ const AboutPage = () => {
       </h2>
       <p className="text-muted" style={{ maxWidth: "600px", margin: "auto" }}>
         Welcome to the Tribe, a fast-growing sex positive community of hot and
-        fun people who are just amazing vibes.
+        fun people who are just amazing vibes. It’s a fantastic space to meet
+        like-minded individuals who are beautiful inside and out, easy going,
+        open, and not non-judgemental.
+        <br />
         <br />
         We run exclusive, invite-only parties of around ninety hand picked
-        guests. We also host monthly socials about ten or so days before the
-        main event so that newbies can get to know each other and make friends
-        ahead of the party.
+        guests. We also host monthly socials where you can get to know each
+        other and make friends before attending a party.
         <br />
-        We also have a single girls chat for any lady who is new to the scene
-        who might feel intimidated coming on their own. There, you can ask
-        questions to the girls who have been before, go for pre-drinks and come
-        to the event later as a group.
         <br />
-        The Tribe is a fantastic space to meet like-minded individuals who are
-        beautiful inside and out, easy going, open, and not non-judgemental.
+        Single girl and feeling a little nervous? We have a separate girls-only
+        chat group – a safe, welcoming space to connect with others who are new
+        or flying solo. Here, you can chat, ask questions to experienced Tribe
+        girls, and even plan meet-ups before the party.
+        <br />
         <br />
         The community is precious to us and we are always looking for kindred
-        spirits.
-        <br />
-        Get in touch if you think you'll fit in. We are excited to getting to
-        know you!
+        spirits. Get in touch if you think you'll fit in. We are excited to get
+        to know you!
       </p>
     </div>
   );
