0d273d6f9dfdd9c1661f94fe06250823061dd04b
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-01-08 21:10:45 +0000

    Change text on main page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -8,14 +8,26 @@ const AboutPage = () => {
         A social experiment in redefining, sex, friendships and relationships
       </h2>
       <p className="text-muted" style={{ maxWidth: "600px", margin: "auto" }}>
-        Welcome to the Tribe, a fast-growing community of hot and fun kinksters.
-        We host exclusive, invite-only parties with over 100 guests. We also
-        host the occasional smaller party which focuses on introducing newbies
-        to the group, as well as monthly socials. The Tribe is a fantastic space
-        to meet like-minded individuals who are beautiful inside and out. The
-        community is precious to us so guests are carefully selected via a
-        voting system but once you're in you're not gonna want to leave. So do
-        get in touch if you think you'll fit in. We are excited to getting to
+        Welcome to the Tribe, a fast-growing sex positive community of hot and
+        fun people who are just amazing vibes.
+        <br />
+        We run exclusive, invite-only parties of around ninety hand picked
+        guests. We also host monthly socials about ten or so days before the
+        main event so that newbies can get to know each other and make friends
+        ahead of the party.
+        <br />
+        We also have a single girls chat for any lady who is new to the scene
+        who might feel intimidated coming on their own. There, you can ask
+        questions to the girls who have been before, go for pre-drinks and come
+        to the event later as a group.
+        <br />
+        The Tribe is a fantastic space to meet like-minded individuals who are
+        beautiful inside and out, easy going, open, and not non-judgemental.
+        <br />
+        The community is precious to us and we are always looking for kindred
+        spirits.
+        <br />
+        Get in touch if you think you'll fit in. We are excited to getting to
         know you!
       </p>
     </div>
