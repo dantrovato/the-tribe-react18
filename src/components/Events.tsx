284af0e52eb0751d9580d3fe07a64d3d049b06c5
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-06-26 21:14:47 +0100

    Add flyers for social and party for july 2025.

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,15 +3,15 @@ import { Footer } from "./Footer";
 const Events = () => {
   return (
     <div className="m-5">
-      <h1 className="text-center text-muted mb-5">Next Events coming soon</h1>
+      <h1 className="text-center text-muted mb-5">Next Events</h1>
 
       <div className="mx-auto text-center" style={{ maxWidth: "80%" }}>
-        {/* <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
+        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
           <h3 className="text-center text-muted mb-4">Play Social</h3>
-          <h4 className="text-center text-muted mb-4">Thursday 5th June </h4>
+          <h4 className="text-center text-muted mb-4">Friday 18th July </h4>
           <img
-            src="june_social2025web.jpg"
-            alt="Attractive people in a bar"
+            src="socialJuly2025web.jpg"
+            alt="Attractive couple flirting at a bar. He is sniffing her neck. What a weirdo!"
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
           />
@@ -20,18 +20,18 @@ const Events = () => {
             style={{ maxWidth: "600px", margin: "auto" }}
           >
             Come and meet the gorgeous children of the tribe at a bar in East
-            London for a midweek social and a little more if the mood strikes
+            London for a Friday social and a little more if the mood strikes.
+            The bar closes at 2 am but the party is likely to continue at some
+            afterparty. Because you just can't get enough of us, can you?!
           </p>
-        </div> */}
+        </div>
 
-        {/* <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">
-            Temple of The Tribe - The Book of More Moans
-          </h3>
-          <h4 className="text-center text-muted mb-4">Saturday 21ST June</h4>
+        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
+          <h3 className="text-center text-muted mb-4">Tribeval III</h3>
+          <h4 className="text-center text-muted mb-4">Saturday 26th July</h4>
           <img
-            src="temple_of_the_tribe-website.jpg"
-            alt="Sexy fire woman in front of a fire truck"
+            src="Tribeval3web.jpg"
+            alt="Sexy fire woman at a festival. She is wearing a red top and has a fire staff."
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
           />
@@ -43,9 +43,9 @@ const Events = () => {
             belly laughs. We will be playing and partying in North London for a
             night of hedonism, fun and, we hope, lasting connections. <br />{" "}
             <br />
-            This is The Tribe. Believe the hype
+            This is The Tribe. Believe the hype.
           </p>
-        </div> */}
+        </div>
 
         <p
           className="text-muted mb-3"
