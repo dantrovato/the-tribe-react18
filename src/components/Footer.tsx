b20056d202205d9f2e4df92b7cbe93e33a7f933d
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-06-30 15:14:33 +0100

    Update footer contacts and insta link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,8 @@ export const Footer = () => {
       <div className="bg-dark">
         <p className="p-3 text-light text-center">
           <small>
-            © thetribeexperiment 2023
+            © thetribeexperiment 2025
+            <br />
             <br />
             Contact:{" "}
             <a
@@ -17,13 +18,17 @@ export const Footer = () => {
               dan@thetribeexperiment.com
             </a>{" "}
             <br />
+            <br />
+            07984290884
+            <br />
+            <br />
             <a
-              href="https://www.instagram.com/thetribehatesinsta"
+              href="https://www.instagram.com/the_tribe_experiment/"
               target="_blank"
               rel="noopener noreferrer"
               style={{ color: "inherit", textDecoration: "none" }} // Add inline style
             >
-              <FontAwesomeIcon icon={faInstagram} /> thetribehatesinsta
+              <FontAwesomeIcon icon={faInstagram} /> the_tribe_experiment
             </a>
           </small>
         </p>
