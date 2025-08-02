948de42159927dc300c0c4456c943cb30b81a82d
Daniele Trovato <plasticsoup@yahoo.co.uk>
2025-08-02 12:27:25 +0100

    Update flyer for misfits august 2025 party

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -6,7 +6,7 @@ const Events = () => {
       <h1 className="text-center text-muted mb-5">Next Events</h1>
 
       <div className="mx-auto text-center" style={{ maxWidth: "80%" }}>
-        <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
+        {/* <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
           <h3 className="text-center text-muted mb-4">Play Social</h3>
           <h4 className="text-center text-muted mb-4">Friday 18th July </h4>
           <img
@@ -24,13 +24,17 @@ const Events = () => {
             The bar closes at 2 am but the party is likely to continue at some
             afterparty. Because you just can't get enough of us, can you?!
           </p>
-        </div>
+        </div> */}
 
         <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">Tribeval III</h3>
-          <h4 className="text-center text-muted mb-4">Saturday 26th July</h4>
+          <h3 className="text-center text-muted mb-4">
+            Misfits and Wrong'uns - Gangbangs of New York
+          </h3>
+          <h4 className="text-center text-muted mb-4">
+            Friday 29th August - North London
+          </h4>
           <img
-            src="tribeval3web.jpg"
+            src="misfits_web.jpg"
             alt="Sexy fire woman at a festival. She is wearing a red top and has a fire staff."
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
@@ -39,10 +43,10 @@ const Events = () => {
             className="text-muted mb-3"
             style={{ maxWidth: "600px", margin: "auto" }}
           >
-            Come and join this silly bunch for a night of music, dancing and
-            belly laughs. We will be playing and partying in North London for a
-            night of hedonism, fun and, we hope, lasting connections. <br />{" "}
-            <br />
+            Come and join this silly bunch for a sexy night of dancing, belly
+            laughs and God knows what else. We will be playing and partying in
+            North London for a night of hedonism, fun and, we hope, lasting
+            connections. <br /> <br />
             This is The Tribe. Believe the hype.
           </p>
         </div>
