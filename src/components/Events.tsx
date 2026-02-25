40054b57c95b22b8e6cb615b4f2cc49f4a1cb143
Daniele Trovato <plasticsoup@yahoo.co.uk>
2026-02-25 15:31:23 +0000

    Add march fliers for social and party

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -10,12 +10,10 @@ const Events = () => {
 
       <div className="mx-auto text-center" style={{ maxWidth: "80%" }}>
         <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
-          <h3 className="text-center text-muted mb-4">Play Social</h3>
-          <h4 className="text-center text-muted mb-4">
-            Thursday 12th February{" "}
-          </h4>
+          <h3 className="text-center text-muted mb-4">Pub Social</h3>
+          <h4 className="text-center text-muted mb-4">Wednesday 11th March </h4>
           <img
-            src="social_feb2026.jpeg"
+            src="march_social2026.jpeg"
             alt="Attractive couple flirting at a bar."
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
@@ -24,18 +22,19 @@ const Events = () => {
             className="text-muted mb-3"
             style={{ maxWidth: "600px", margin: "auto" }}
           >
-            Come and meet the gorgeous children of the tribe at a bar in East
-            London for a Thursday social and a little more if the mood strikes.
+            Come and meet the gorgeous children of The Tribe at a pub in
+            Mornington Crescent for a Wednesday social with great chats and
+            lotsa bantz.
           </p>
         </div>
 
         <div className="text-muted mb-5" style={{ textAlign: "justify" }}>
           <h3 className="text-center text-muted mb-4">Chavs and Royals</h3>
           <h4 className="text-center text-muted mb-4">
-            Friday 20th February - Manor House, London
+            Friday 20th March - Dalston Junction, London
           </h4>
           <img
-            src="chavs_and_royals_web.jpeg"
+            src="hot_cool_wet_dirty.jpeg"
             alt="Sexy people at a wedding"
             className="mx-auto d-block mb-4"
             style={{ maxWidth: "100%", marginTop: "2rem" }}
@@ -46,7 +45,7 @@ const Events = () => {
           >
             Come and join this silly bunch for a sexy night of dancing, belly
             laughs and God knows what else. We will be playing and partying in
-            North London for a night of hedonism, fun and, we hope, lasting
+            East London for a night of hedonism, fun and, we hope, lasting
             connections. <br /> <br />
             This is The Tribe. Believe the hype.
           </p>
