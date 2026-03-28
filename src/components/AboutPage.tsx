ec1a71c3f742d77b0c93e3083a48ec5433bdf966
Daniele Trovato <plasticsoup@yahoo.co.uk>
2026-03-28 14:57:23 +0000

    Update the about on the home page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -5,32 +5,33 @@ const AboutPage = () => {
         className="text-muted text-center mb-4"
         style={{ maxWidth: "600px", margin: "auto" }}
       >
-        A social experiment in redefining, sex, friendships and relationships
+        A social experiment in redefining sex, friendships and relationships
       </h2>
       <p
         className="text-muted text-center"
         style={{ maxWidth: "600px", margin: "auto" }}
       >
-        Welcome to the Tribe, a fast-growing sex positive community of hot and
-        fun people who are just amazing vibes. It’s a fantastic space to meet
-        like-minded individuals who are beautiful inside and out, easy going,
-        open, and non-judgemental.
+        Welcome to the Tribe, a no-drama community of exquisite people centred
+        around an epic monthly party built on trust, humour and connection.
         <br />
         <br />
-        We run exclusive, invite-only parties of around ninety hand picked
-        guests. We also host monthly socials where you can get to know each
-        other and make friends before attending a party.
+        This is a brilliant space to meet like-minded individuals who are
+        beautiful, inside and out.
         <br />
         <br />
-        Single girl and feeling a little nervous? We have a separate girls-only
-        chat group – a safe, welcoming space to connect with others who are new
-        or flying solo. Here, you can chat, ask questions to experienced Tribe
-        girls, and even plan meet-ups before the party.
+        We believe you become the average of those around you, so we choose to
+        surround ourselves with people committed to being their best selves.
+        <br />
+        This is not just a party. You’re going on a date, not with one person,
+        but with an entire group.
         <br />
         <br />
-        The community is precious to us and we are always looking for kindred
-        spirits. Get in touch if you think you'll fit in. We are excited to get
-        to know you!
+        We are a social revolution; the paradigm shift.
+        <br />
+        <br />
+        This is The Tribe.
+        <br />
+        Believe the hype.
       </p>
     </div>
   );
